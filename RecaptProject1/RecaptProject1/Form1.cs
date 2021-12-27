768fbbffb1a891d4e2355cb80b542c8caa31dfd6
Betül Orhan <84873320+betulorhan@users.noreply.github.com>
2021-12-27 23:06:05 +0300

    Add files via upload

---
+++ RecaptProject1/RecaptProject1/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RecaptProject1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ListProducts();
+            ListCategories();   
+        }
+
+        private void ListProducts()
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                dgwProduct.DataSource = context.Products.ToList(); // Veritabanındaki products tablosunu datagriedviewde işleme 
+            }
+        }
+
+        private void ListCategories()
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                cbxCategory.DataSource = context.Categories.ToList();   
+                cbxCategory.DisplayMember = "CategoryName"; // Categoryde kayıtlı isimleri seçtik
+                cbxCategory.ValueMember = "CategoryId"; // Seçili categoryi idye göre getir.
+            }
+        }
+
+        private void ListProductsByCategoryId(int categoryId)
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                dgwProduct.DataSource = context.Products.Where(p=>p.CategoryId == categoryId).ToList();
+                // Ürünleri category idleri, parametre olarak verilen categoryId ile aynı olanlara göre listele. 
+            }
+        }
+
+        private void cbxCategory_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // MessageBox.Show(cbxCategory.SelectedValue.ToString()); // CBX'tan seçilen ürüne göre, ürüne ait idyi mesaj olarak yazdırdık. 
+
+            try
+            {
+                ListProductsByCategoryId(Convert.ToInt32(cbxCategory.SelectedValue));
+            }
+            catch
+            {
+
+            }
+            
+        }
+    }
+}
---
+++ RecaptProject1/RecaptProject1/NorthwindContext.cs
@@ -0,0 +1,16 @@
+﻿using RecaptProject1.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecaptProject1
+{
+    public class NorthwindContext : DbContext
+    {        
+        public DbSet<Product> Products { get; set; } // Veritabanındaki Products tablosu nesne olan Product ile eşleiştirildi.
+        public DbSet<Category> Categories { get; set; } // Category sınıfındaki nesneleri contexte çektik.
+    }
+}
